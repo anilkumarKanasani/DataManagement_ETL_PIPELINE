3c864e1c0fc8f1a55dff5fd286866086374a0089
Anil Kumar <anilkumar.kanasani@gmail.com>
2020-11-04 14:49:23 +0100

    S3 bucket (dumping-data-lake) is implemented in the main terraform file

--- aws_code/terraform/main.tf
+++ aws_code/terraform/main.tf
@@ -1,7 +1,14 @@
+# AWS crenditials will be fetched from Windows Environment variables.
+# https://stackoverflow.com/questions/55052153/how-to-configure-environment-variables-in-hashicorp-terraform#:~:text=You%20can%20provide%20your%20credentials,and%20AWS%20Secret%20Key%2C%20respectively.&text=In%20the%20more%20general%20case,that%20are%20prefixed%20with%20TF_VAR_%20.
+
+
 provider "aws" {
     region = "eu-central-1"
 }
 
+#############################################################################################################################
+
+# Creating VPC and SUBNET resources using vpc module in tf_Module Folder.
 module "ETL_VPC" {
     source = "./tf_Modules/vpc"
 
@@ -20,6 +27,9 @@ module "ETL_VPC" {
 }
 
 
+#############################################################################################################################
+
+# Creating VPC and SUBNET resources using vpc module in tf_Module Folder.
 module "ETL_machine" {
     source = "./tf_Modules/ec2"
 
@@ -35,5 +45,21 @@ module "ETL_machine" {
 
     # No Default value
     ec2_subnet_id = "${module.ETL_VPC.subnet_id}" 
+}
+
+
+#############################################################################################################################
+
+# Creating S3 Bucket for Dumping CSV Data from different Data Sources
+module "ETL_bucket" {
+    source = "./tf_Modules/s3"
+
+    # No Default Value
+    bucket_name = "dumped-data-lake"
+
+    # No Default value
+
+    bucket_tag = "dumped-data-lake" 
+    
 }
     
