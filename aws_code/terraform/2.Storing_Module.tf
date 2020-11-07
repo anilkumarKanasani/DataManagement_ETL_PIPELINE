4c0692d289009e24dd80e69a7f8a3f70111f7344
Anil Kumar <anilkumar.kanasani@gmail.com>
2020-11-07 08:43:29 +0100

    Provider is removed from all modules

--- aws_code/terraform/2.Storing_Module.tf
+++ aws_code/terraform/2.Storing_Module.tf
@@ -2,10 +2,6 @@
 # https://stackoverflow.com/questions/55052153/how-to-configure-environment-variables-in-hashicorp-terraform#:~:text=You%20can%20provide%20your%20credentials,and%20AWS%20Secret%20Key%2C%20respectively.&text=In%20the%20more%20general%20case,that%20are%20prefixed%20with%20TF_VAR_%20.
 
 
-provider "aws" {
-    region = "eu-central-1"
-}
-
 #############################################################################################################################
 
 # Creating DynamoDB Tables for data injecting from dumped-data-lake S3 Bucket through lambda function
